fe96fae3cdaa2c1b644bde317653cd14cbfb5d4f
Ian Macalinao <ian@saber.so>
2021-09-02 17:24:26 -0500

    Add built-in tokenaccount and mint validations

--- src/validate.rs
+++ src/validate.rs
@@ -1,9 +1,37 @@
 //! Validations for accounts.
 
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, TokenAccount};
 
-/// Validator for [Accounts] structs.
-pub trait Validate<'info>: Accounts<'info> {
+use crate::assert_owner;
+
+/// Validates the contents of a variable. Generally used for [Accounts] structs and struct members.
+pub trait Validate<'info> {
     /// Validates the account struct.
     fn validate(&self) -> ProgramResult;
+}
+
+/// Represents an account owned by a program.
+pub trait ProgramOwned: AccountDeserialize + Clone {
+    /// Gets the expected owner of the program owned account.
+    fn expected_owner(&self) -> &'static Pubkey;
+}
+
+impl<'info, T: ProgramOwned> Validate<'info> for CpiAccount<'info, T> {
+    fn validate(&self) -> ProgramResult {
+        assert_owner!(*self, *self.expected_owner());
+        Ok(())
+    }
+}
+
+impl ProgramOwned for TokenAccount {
+    fn expected_owner(&self) -> &'static Pubkey {
+        &crate::program_ids::token::ID
+    }
+}
+
+impl ProgramOwned for Mint {
+    fn expected_owner(&self) -> &'static Pubkey {
+        &crate::program_ids::token::ID
+    }
 }
