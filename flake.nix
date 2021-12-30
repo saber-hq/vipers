b7c4ea70dc320a7bfa1433bd8047102c30453bf1
Ian Macalinao <ian@saber.so>
2021-12-29 21:09:15 -0800

    Update deps, set up Nix development environment

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Saber Vipers development environment.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell { buildInputs = with pkgs; [ libiconv ]; };
+      });
+}
