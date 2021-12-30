3e9645ee5e61d3b62919cd5d60247fb357753c41
Ian Macalinao <ian@saber.so>
2021-12-30 00:03:00 -0800

    Set up cargo workspaceS

--- flake.nix
+++ flake.nix
@@ -3,13 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    saber-overlay.url = "github:saber-hq/saber-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, saber-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; }
+          // saber-overlay.packages.${system};
       in {
-        devShell = pkgs.mkShell { buildInputs = with pkgs; [ libiconv ]; };
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ libiconv anchor-0_19_0 cargo-workspaces ];
+        };
       });
 }
