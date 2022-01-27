0bcf5299d16ad60c5467c2d4cb7a594fae63710b
Ian Macalinao <ian@saber.so>
2022-01-27 12:36:51 -0800

    check references in validate_derived_address

--- vipers/src/lib.rs
+++ vipers/src/lib.rs
@@ -14,13 +14,26 @@ pub use validate::Validate;
 declare_id!("VipersTest111111111111111111111111111111111");
 
 /// Validates a derived program address.
+///
+/// # Example
+///
+/// ```
+/// use vipers::validate_derived_address;
+/// use anchor_lang::solana_program;
+/// let random = solana_program::system_program::ID;
+/// let seeds: &[&[u8]] = &["test".as_ref() as &[u8], &random.to_bytes()];
+/// let expected = static_pubkey::static_pubkey!("HjTCk2QYVrDPH1emJyrKBjtnooGqTvHfxa8ResZg3Kb4");
+/// assert!(validate_derived_address(
+///   &expected, &vipers::ID, seeds
+/// ));
+/// ```
 pub fn validate_derived_address(
     derived_address: &Pubkey,
     program_id: &Pubkey,
     seeds: &[&[u8]],
 ) -> bool {
     match Pubkey::create_program_address(seeds, program_id) {
-        Ok(key) => *derived_address == key,
+        Ok(ref key) => derived_address == key,
         _ => false,
     }
 }
@@ -30,7 +43,7 @@ pub mod prelude {
 
     pub use super::{
         assert_is_zero_token_account, assert_keys_eq, assert_keys_neq, invariant, try_or_err,
-        unwrap_int, unwrap_opt, unwrap_or_err, Validate,
+        unwrap_int, unwrap_opt, unwrap_opt_block, unwrap_or_err, Validate,
     };
 }
 
